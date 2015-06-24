a92373c1f6aa5957d9f4c4fb375c5fec334f4317
unknown <ldecicco@usgs.gov>
2015-06-24 11:11:47 -0500

    Package skeleton.

--- R/gsplot-class.R
+++ R/gsplot-class.R
@@ -1,6 +1,20 @@
-
-
-gsplot <- function(list_obj){
-  class(list_obj) <- "gsplot"
-  invisible(list_obj)
+#' gsplot
+#'
+#' Class stuff
+#'
+#' @param x list
+#' @return gsplot 
+#' @export
+#' @examples
+#' gsplot(list())
+gsplot <- function(x){
+  UseMethod("gsplot", x)
+  invisible(x)
 }
+
+#' @export
+gsplot.list <- function(x){
+  class(x) <- "gsplot"
+  invisible(x) 
+}
+
