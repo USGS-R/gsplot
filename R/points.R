e04043148fe1d50074a11481cc322b3458beb972
unknown <ldecicco@usgs.gov>
2015-06-24 11:44:51 -0500

    Points!

---
+++ R/points.R
@@ -0,0 +1,23 @@
+#' gsplot points
+#'
+#' Point stuff
+#'
+#' @param gsplot gsplot object
+#' @param \dots
+#' @return modified gsplot object 
+#' @export
+#' @examples
+#' gs <- gsplot(list(figure="testFig"))
+#' gsNew <- points(gs, x=1, y=2)
+points <- function(...){
+  dots <- list(...)
+  
+  if(length(dots) != 0 && class(dots[[1]]) == "gsplot" ){
+    gsplot <- dots[[1]]
+    gsplot$points <- dots[-1]
+    return(gsplot)    
+  } else {
+    graphics::points(...)
+  }
+
+}
