65ef2dff649f9ddc9128dd4bd0ca28ddcace11dc
Jordan S Read <jread@usgs.gov>
2015-06-25 11:18:18 -0500

    man warning fix for %>%

--- R/import_export.R
+++ R/import_export.R
@@ -2,6 +2,7 @@
 #' 
 #' using the magrittr pipe (lhs \%>\% rhs) to chain operations
 #' @importFrom magrittr %>%
+#' @param lhs,rhs A dataset and function to apply to it
 #' @name %>%
 #' @export
 #' @rdname builder
