13b798045835c63cf2b13d6feb709dd59e6edddb
Jordan S Read <jread@usgs.gov>
2015-06-25 11:05:53 -0500

    builder workflow for #24

---
+++ R/import_export.R
@@ -0,0 +1,9 @@
+#' Chain together multiple operations.
+#' 
+#' using the magrittr pipe (lhs \%>\% rhs) to chain operations
+#' @importFrom magrittr %>%
+#' @name %>%
+#' @export
+#' @rdname builder
+#' @usage lhs \%>\% rhs
+NULL
