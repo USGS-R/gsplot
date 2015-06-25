c64563bb11de25b908e729ea68858ece42bae605
thongsav-usgs <thongsav@usgs.gov>
2015-06-25 11:21:47 -0500

    Preliminary start to legend. Does NOT handle defaulting and will error out if you do not specify info for points and lines.

--- R/legend.R
+++ R/legend.R
@@ -1,42 +1,91 @@
 #' gsplot legend
 #'
-#' If called with gsplot as first argument, will cycle through the gsplot settings 
-#' (points, lines, etc) and will produce a legend data structure to be added to the 
-#' plot when it is rendered.
+#' If called with gsplot as first argument, will set the internal gsplot configuration
+#' for legends
 #'
 #' @param legend.location specifies where the legend is relative to the plot. 
 #'  Can use standard location parameter ("bottomright", "bottom", "bottomleft", "left", 
 #'  "topleft", "top", "topright", "right" and "center") with the additions "below", "above", 
 #'  "out-left", "out-right".
-#' @param \dots stuff
+#' @param \dots normal legend params should forward through
 #' @return modified gsplot object 
 #' @export
 #' @examples
 #' gs <- gsplot(list(figure="testFig"))
-#' gs <- points(gs, x=1, y=2, legend.name="Example Points", pch=1, col="blue")
-#' gs <- legend(gs, location="topleft")
-legend <- function(object, location="topright", ...){
-  if (!missing(object) && class(object) == "gsplot" ){
-    object$legend <- list(location = location, ...)
-    return(object)
-  } else {
-    if (missing(object)){
-      graphics::legend(...)
-    } else {
-      graphics::legend(object, ...)
-    }
-  }
+#' gs <- points(gs, x=1, y=2, legend.name="Example Points 1", pch=1, col="blue")
+#' gs <- points(gs, x=3, y=4, legend.name="Example Points 2", pch=5, col="red")
+#' gs <- lines(gs, x=c(3,4,3), y=c(2,4,6), legend.name="Example Line", lty=1, col="orange")
+#' gs <- legend(gs)
+legend <- function(object, ...){
+  overrideGraphics("legend", object, ...)
 }
+
+
+legend.gsplot <- function(object, location="topright", ...) {
+  object <- append(object, list(legend = list(x = location, ...)))
+  return(gsplot(object))
+}
+
 #' gsplot draw_legend
 #'
 #' Will cycle through  the gsplot, looking at the legend configuration (if one exists), and
 #' create legend entries based on the points, lines, etc. contained in the gsplot.
 #' 
-#' @param gsplot the gsplot to render legend on
+#' @param gsplot the gsplot to render legend
 
 draw_legend <- function(gsplot) {
-  location <- gsplot[['legend']]$location
-  legend(x=gsplot$legend$location, y=NULL, "YAAAAAAAAY", pch = 1, col = c(1, 3), cex = 1.2)
+  legendParams <- gsplot[['legend']]
+  if(!is.null(legend)) {
+    smartLegend <- data.frame(text = character(), 
+                              symbol = numeric(), 
+                              color = character(), 
+                              line = numeric(), 
+                              stringsAsFactors = FALSE)
+    #get legend entries for points
+    pts_i <- which(names(gsplot) %in% 'points')
+    for (i in pts_i){
+      pts <- gs[[i]]
+      smartLegend <- rbind(smartLegend, data.frame(text = pts$legend.name, 
+                                                   symbol = pts$pch, 
+                                                   color = pts$col, 
+                                                   line = NA, 
+                                                   stringsAsFactors = FALSE))
+    }
+    
+    #get legend entries for lines
+    lines_i <- which(names(gsplot) %in% 'lines')
+    for (i in lines_i){
+      lines <- gs[[i]]
+      smartLegend <- rbind(smartLegend, data.frame(text = lines$legend.name, 
+                                                   symbol = NA, 
+                                                   color = lines$col, 
+                                                   line = lines$lty, 
+                                                   stringsAsFactors = FALSE))
+    }
+    
+    #only include pch if we have a non-NA entry for points
+    if(length(pts_i) > 0) {
+      legendParams <- append(legendParams, list(
+        pch=smartLegend$symbol
+      ))
+    }
+    
+    #only include pch if we have a non-NA entry for points
+    if(length(lines_i) > 0) {
+      legendParams <- append(legendParams, list(
+        lty=smartLegend$line
+      ))
+    }
+    
+    legendParams <- append(legendParams, list(
+      legend=smartLegend$text, 
+      col=smartLegend$color
+      ))
+    
+    legend(legendParams,
+           #x = location,  do we want to support explicit x/y coords? 
+           y = NULL) 
+  }
 }
 
 
