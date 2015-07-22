f01c900dc1e0c76e811f420221a7fa5dcfdf0225
Laura DeCicco <ldecicco@usgs.gov>
2015-07-22 11:53:21 -0500

    Made bars for NA and 0 go away.

--- R/error_bars.R
+++ R/error_bars.R
@@ -53,16 +53,33 @@ error_bar.default <- function(x, y, y.high=0, y.low=0, x.high=0, x.low=0, epsilo
   x.high[is.na(x.high)] <- 0
   x.low[is.na(x.low)] <- 0
   
+  ep.y.low <- epsilon
+  ep.y.high <- epsilon
+  ep.x.low <- epsilon
+  ep.x.high <- epsilon
+  
+  if(length(epsilon) == 1){
+    ep.y.low <- rep(epsilon,length(y.low))
+    ep.y.high <- rep(epsilon,length(y.high))
+    ep.x.low <- rep(epsilon,length(x.low))
+    ep.x.high <- rep(epsilon,length(x.high))
+  } 
+  
+  ep.y.low[y.low == 0] <- 0
+  ep.y.high[y.high == 0] <- 0
+  ep.x.low[x.low == 0] <- 0
+  ep.x.high[x.high == 0] <- 0
+  
   if(!all(y.low == 0) && !all(y.high == 0)){
     segments(x, y-y.low,x, y+y.high, ...)
-    segments(x-epsilon,y-y.low,x+epsilon,y-y.low, ...)
-    segments(x-epsilon,y+y.high,x+epsilon,y+y.high, ...)    
+    segments(x-ep.y.low, y-y.low, x+ep.y.low,y-y.low, ...)
+    segments(x-ep.y.high,y+y.high,x+ep.y.high,y+y.high, ...)    
   }
 
   if(!all(x.low == 0) && !all(x.high == 0)){
     segments(x-x.low, y, x+x.high, y, ...)
-    segments(x-x.low, y-epsilon,x-x.low,y+epsilon, ...)
-    segments(x+x.high, y-epsilon,x+x.high,y+epsilon, ...) 
+    segments(x-x.low, y-ep.x.low,x-x.low,y+ep.x.low, ...)
+    segments(x+x.high, y-ep.x.high,x+x.high,y+ep.x.high, ...) 
   }
 
 }
