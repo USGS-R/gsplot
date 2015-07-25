78e6123dbc15f2f65b9c7a8b63c0f7ff8b67e341
Jordan S Read <jread@usgs.gov>
2015-07-25 08:33:27 -0500

    additional tests

--- tests/testthat/tests-par.R
+++ tests/testthat/tests-par.R
@@ -9,6 +9,15 @@ test_that("lists and named args are identical", {
   
 })
 
-test_that("empty sets on gsplot are ignored",{
+test_that("a more complicated par set", {
+  gs <- gsplot(cex=1.2) %>% 
+    par(cex=3.2) %>%
+    par(cex=3.8,mar=c(4,4,4,4)) %>% 
+    points(3,2,cex=2)
+  expect_equal(gs[['par']][['cex']],3.8)
+})
+
+test_that("par is a list",{
+  gs <- gsplot(cex=1.2)
   expect_is(gs[['par']], 'list')
 })
