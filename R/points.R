2015d71299efd51b745c33fa60f48a8d1db591e4
Jordan S Read <jread@usgs.gov>
2015-09-10 12:33:52 -0500

    adding lines and test for user args not being replaced

--- R/points.R
+++ R/points.R
@@ -53,7 +53,7 @@ points.gsplot <- function(object, ..., legend.name=NULL, side=c(1,2)){
 
   object <- gsplot(append(object, to.gsplot)) # append initial call
   if (!is.null(e.fun)){
-    embed.args = set_args(e.fun,c(arguments, dots$e.args), package = 'gsplot')
+    embed.args = set_args(e.fun,c(arguments[!names(arguments) %in% names(dots$e.args)], dots$e.args), package = 'gsplot')
     object <- do.call(e.fun, append(list(object=object), embed.args))
   }
   return(object)
--- tests/testthat/tests-embed_function.R
+++ tests/testthat/tests-embed_function.R
@@ -6,3 +6,17 @@ test_that("points to callouts", {
   expect_equal(names(gs$view),c('points','callouts','window'))
 
 })
+
+test_that("lines to callouts", {
+  
+  gs <- lines(gsplot(), c(0,3), c(2,4), callouts(labels=c('dogs','cats')))
+  expect_equal(names(gs$view),c('lines','callouts','window'))
+  
+})
+
+test_that("usr args aren't overidden", {
+  
+  gs <- lines(gsplot(), c(0,3), c(2,4), callouts(labels=c('dogs','cats'), col='yellow'))
+  expect_equal(gs$view$callouts$col, 'yellow')
+  
+})
