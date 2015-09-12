b879c01897d3b88a62b97721c5f7dabfe950d749
Jordan S Read <jread@usgs.gov>
2015-09-12 09:38:48 -0500

    test

--- tests/testthat/tests-callouts.R
+++ tests/testthat/tests-callouts.R
@@ -4,9 +4,6 @@ test_that("lines to callouts", {
   expect_warning(callouts(gsplot(), c(0,3), NULL, labels=c('dogs','cats')))
   expect_warning(callouts(gsplot(), c(0,3), y=NULL, labels=c('dogs','cats')))
   
-  #expect_error(callouts(gsplot(), x=c(0,3), y=1:2, angle=-30))
-  #expect_error(callouts(gsplot(), x=c(0,3), y=1:2, angle='random'))
-  
   callouts(gsplot(), c(0,3), y=1:2, labels=c('dogs','cats'), angle=30)
   callouts(gsplot(), c(0,3), y=1:2, labels=c('dogs','cats'), angle='auto')
   
