0c55323476a87d2d6e450e1add3ad3f5db0e186b
Lindsay Carr <lcarr@usgs.gov>
2015-12-09 08:38:49 -0600

    mtext tests parentheses fix

--- tests/testthat/test-mtext.R
+++ tests/testthat/test-mtext.R
@@ -11,7 +11,7 @@ test_that("mtext on correct side", {
   expect_false(gs[[i]][['window']][['axes']])
 })
 
-test_that("mtext las are defined for both mtext calls", {
+test_that("mtext las defined for both mtext calls", {
   gs <- gsplot() %>% 
     points(1,2) %>% 
     mtext(text=c(1,2,3,4), at=c(0.7,0.9,1.1,1.3), cex=0.5, las=2, side=1, line=1) %>% 
@@ -28,8 +28,8 @@ test_that("multiple mtext are on correct sides", {
     mtext(text=c("yr1", "yr2"), at=c(0.8, 1.2), las=1, side=3, line=3)
   
   expect_true(any(gs[[1]][['window']][['side']] %in% 1))
-  expect_true(any(names(gs[[1]] %in% "mtext")))
+  expect_true(any(names(gs[[1]]) %in% "mtext"))
   
   expect_true(any(gs[[2]][['window']][['side']] %in% 3))
-  expect_true(any(names(gs[[2]] %in% "mtext")))
+  expect_true(any(names(gs[[2]]) %in% "mtext"))
 })
