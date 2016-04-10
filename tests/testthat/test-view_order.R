6b351930de04ccbbced4be2c81ed9911d1520550
Jordan S Read <jread@usgs.gov>
2016-04-10 08:39:32 -0500

    view order

--- tests/testthat/test-view_order.R
+++ tests/testthat/test-view_order.R
@@ -16,7 +16,7 @@ test_that("multiple view order is retained", {
   
   gs <- points(gs, c(0,3), c(2,4)) %>% 
     lines(c(0,3), c(2,4), side=c(3,4))
-  
-  expect_equal(names(gs[[1]]),c('points','window'))
-  expect_equal(names(gs[[2]]),c('lines','window'))
+  views <- gsplot:::views(gs)
+  expect_equal(names(views[[1]]),c('points','window'))
+  expect_equal(names(views[[2]]),c('lines','window'))
 })
