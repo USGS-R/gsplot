b62e94e609fbd4ba65a118dc8ec15a7b684a7ea1
Jordan Walker <jiwalker@usgs.gov>
2016-04-18 12:32:57 -0500

    Stubbed out, currently does nothing

---
+++ R/modify_side.R
@@ -0,0 +1,10 @@
+#' modify a side to update it with new arguments
+#' 
+#' @param object gsplot object
+#' @param side sides to modify with extra parameters
+#' @param \dots user arguments to passed through
+#' 
+#' @keywords internal
+modify_side <- function(object, side, ...) {
+  return(object)
+}
