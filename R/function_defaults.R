de08b37a35f8199f41605e8ec3babb4454da0e0c
Jordan S Read <jread@usgs.gov>
2016-04-18 19:09:20 -0500

    test and docs

--- R/function_defaults.R
+++ R/function_defaults.R
@@ -1,4 +1,10 @@
-
+#' get the defaults for a function name 
+#' 
+#' parent package for function, and default functions for capturing arguments
+#' 
+#' @param fun.name a chacter vector (length 1) for the function name
+#' @param out the exports you want (defaults to 'package' & 'def.funs')
+#' @return a list if more than one output is selected, unlisted if length one
 function_defaults <- function(fun.name, out=c('package','def.funs')){
   stopifnot(length(fun.name) == 1)
   funs <- list('points'=
