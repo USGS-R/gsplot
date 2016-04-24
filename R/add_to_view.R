820191f4b1e02ff74d0efdbeb2b90d57a498bbd0
Jordan S Read <jread@usgs.gov>
2016-04-24 10:07:30 -0500

    adding default par from config to initialize

--- R/add_to_view.R
+++ R/add_to_view.R
@@ -7,6 +7,7 @@ add_new_view <- function(object, view.name){
   
   last.view.i <- max(which_views(object), 0)
   object <- append(object, view, after = last.view.i)
+  object <- modify_view_par(object, config('par'), side=as.side(view.name))
   return(object)
 }
 #' add function call to view
