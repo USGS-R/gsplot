011db740b043f4df287a27b49f9efcb26b1974a2
Jordan S Read <jread@usgs.gov>
2016-05-02 20:59:31 -0500

    cleaning

--- R/calc_views.R
+++ R/calc_views.R
@@ -8,38 +8,6 @@ append_replace <- function(old.list, new.list){
 }
 
 
-which_reals <- function(values, na.value){
-  
-  if (is.na(na.value))
-    return(which(!is.na(values)))
-  else
-    return(which(!is.na(values) & values != na.value)) # which row to use. goofy because values != NA is always NA, not logical
-  
-}
-set_view_window <- function(views, param, na.value=NA, remove=TRUE, ignore=NULL){
-  view_i <- which_views(views)
-  for (i in view_i){
-    values <- lapply(views[[i]][!names(views[[i]]) %in% ignore], function(x) strip_pts(x, param))
-    val.i <- which_reals(values, na.value)
-    if (length(val.i) == 0){
-      values = na.value
-    } else {
-      values <- unname_c(values[val.i])
-    }
-    if (remove)
-      views[[i]] <- lapply(views[[i]], function(x) remove_field(x, param))
-    
-    views[[i]][['window']][[param]] <- values
-  }
-  
-  return(views)
-}
-
-
-set_view_lab <- function(views){
-  views <- set_view_window(views, param = 'ylab', na.value="")
-  set_view_window(views, param = 'xlab', na.value="")
-}
 
 c_unname <- function(list){
   unname(do.call(c, list))
@@ -100,50 +68,3 @@ strip_pts2 <- function(data, param) {
   return(out)
 }
 
-set_window <- function(list){
-  
-  listOut <- list
-  pars <- list[['par']]
-  
-  for(j in which_views(list)){
-    
-    window <- list[[j]][['window']]
-    plots <- list[[j]]
-    plots[['window']] <- NULL
-    
-    param <- c("axes","ann","frame.plot") #Add panel.first, panel.last, asp, and "main","sub","frame.plot"...without breaking title
-    varPar <- c("xaxs","yaxs","xaxt","yaxt","las")
-    
-    window[param[!(param %in% names(window))]] <- TRUE
-    
-    for(i in names(plots)){
-      for(k in param){
-        if(k %in% names(plots[[i]])){
-          window[[k]] <- plots[[i]][[k]]
-        } 
-        plots[[i]][[k]] <- NULL
-      }
-      pars[varPar[varPar %in% names(plots[[i]])]] <- plots[[i]][names(plots[[i]]) %in% varPar]
-      plots[[i]][names(plots[[i]]) %in% varPar] <- NULL
-    }
-    
-    for (h in which(names(list) == "axis")) {
-      if(list[[h]][['arguments']][['side']] %in% window[['side']]) {
-        reverse <- list[[h]][['gs.config']][['reverse']]
-        if (!is.null(reverse) && reverse ) {
-          sideToReverse <- list[[h]][['arguments']][['side']]
-          axes <- ifelse(sideToReverse %% 2 == 0, 'y', 'x')
-          axisReverse <- paste0(axes, "lim")
-          window[[axisReverse]] <- rev(window[[axisReverse]])
-        }
-      }
-    }
-    
-    listOut[[j]] <- plots
-    listOut[[j]][['window']] <- window
-  }
-  
-  listOut[['par']] <- pars
-  
-  return(listOut)
-}
