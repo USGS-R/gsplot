bcb0d6ae217ae3f3a8edc73a20857d1880623ec2
Lindsay Carr <lcarr@usgs.gov>
2016-06-30 15:06:36 -0500

    update axis tests

--- tests/testthat/tests-axis.R
+++ tests/testthat/tests-axis.R
@@ -41,7 +41,8 @@ test_that("axis reverse",{
     axis(3, reverse=TRUE)
   
   expect_true(gs2$side.3$reverse)
-  expect_equal(ylim(gs2, side=3), c(10,1))
+  expect_equal(xlim(gs2, side=3), c(10,1))
+  expect_error(ylim(gs2, side=3))
   
 })
 
