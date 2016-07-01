048860ab07af5b7739c69f2003c6ad063136cba0
Jordan Walker <jiwalker@usgs.gov>
2016-07-01 14:22:49 -0500

    Fix tests

--- tests/testthat/test-legend_args.R
+++ tests/testthat/test-legend_args.R
@@ -5,5 +5,5 @@ test_that("legend user args", {
     points(x=1, y=2, side=c(3,2), legend.name="Points 1", cex=3, xlab='cat', col='dogerblue') %>% 
     lines(x=c(3,4,3), y=c(2,4,6), legend.name="Lines 1", lty=5, col="orange") %>% 
     legend()
-  expect_equal(usrDef$legend$legend.args$col, c('dogerblue','orange'))
+  expect_equal(usrDef$legend$legend.auto$col, c('dogerblue','orange'))
 })
