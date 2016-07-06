dde4df6bb0d7ab5521572415261970121d325e71
Jordan S Read <jread@usgs.gov>
2016-07-06 08:24:59 -0500

    more checks

--- R/mutate-gsplot.R
+++ R/mutate-gsplot.R
@@ -4,14 +4,23 @@
 #' set the lim for views in gsplot object
 #' 
 #' @param object a gsplot object
-#' @param value the new limits to use (vector)
-#' @param side which side(s) to use
+#' @param value the new limits to use (vector), or a named list
+#' @param side which side(s) to use (NULL when value is a list)
 #' 
 #' @examples 
 #' myplot <- gsplot() %>% 
 #'   points(1:3, 2:4) %>%
 #'   lines(1:5, 5:1)
+#' # numeric side:
 #' lim(myplot, side=1)<-c(1,4)
+#' 
+#' # named side:
+#' lim(myplot, 'side.1')<-c(1.1,3.9)
+#' lim(myplot, 1)
+#' 
+#' # named list
+#' lim(myplot) <- list('side.1' = c(1.1,3.9))
+#' 
 #' myplot
 #' @export
 `lim<-` <- function(object, value, side) UseMethod('lim<-', object)
@@ -20,16 +29,41 @@
 `lim<-.gsplot` <- function(object, value, side=NULL){
   if (is.null(side)){
     # // value must be a list
-    stop('list value for `lim<-.gsplot` is not implemented')
+    stopifnot(is(value,'list'))
+    object <- `lim<-.gsplot_list`(object, value)
   } else {
-    # // side needs to be length 1, unless we deal w/ it
     # // deal w/ setting reverse back to FALSE?
-    # // hack to deal w/ lim(myplot, side=1)[1] <- 0.25 case w usr.lim setting
-    set.i <- !is.na(value) & lim(object, side=side) != value
-    object[[as.side_name(side)]]$lim[set.i] <- value[set.i]
-    object[[as.side_name(side)]]$usr.lim <- set.i
+    if (is(value, 'list')){
+      stop('cannot use value as a list and specify side argument', call. = FALSE)
+    }
+    
+    if (is(side,'numeric')){
+      side.name <- as.side_name(side)
+    } else if (is(side,'character')){
+      side.name <- side
+    } else {
+      stop('class', class(side), 'for side is not supported')
+    }
+
+    object <- `lim<-.gsplot_named_side`(object, value, side.name)
     
   }
+  return(object)
+}
+
+`lim<-.gsplot_list` <- function(object, value){
+  side.names <- names(value)
+  for (side.name in side.names){
+    object <- `lim<-.gsplot_named_side`(object, value=value[[side.name]], side.name)
+  }
+  return(object)
+}
+`lim<-.gsplot_named_side` <- function(object, value, side.name){
+  stopifnot(length(side.name) == 1, is(side.name,'character'), side.name %in% side_names(object))
+  # // hack to deal w/ lim(myplot, side=1)[1] <- 0.25 case w usr.lim setting
+  set.i <- !is.na(value) & lim(object, side=side.name) != value
+  object[[side.name]]$lim[set.i] <- value[set.i]
+  object[[side.name]]$usr.lim <- set.i
   return(object)
 }
 
--- tests/testthat/test-mutate_gsplot.R
+++ tests/testthat/test-mutate_gsplot.R
@@ -22,8 +22,6 @@ test_that("set lim usr.lim", {
   lim(usrDef, side=3)[1] <- 0.2
   expect_true(usrDef$side.3$usr.lim[1])
   expect_false(usrDef$side.3$usr.lim[2])
-  
-  warning("should test that you can't set a limit for a side that doesn't exist")
 })
 
 test_that("set lim w/ NA", {
@@ -36,4 +34,27 @@ test_that("set lim w/ NA", {
   expect_false(usrDef$side.3$usr.lim[2])
   usrDef <- points(usrDef, 5,2, side=c(3,2))
   expect_equal(lim(usrDef, 3)[2], 5)
+})
+
+test_that("can set limits with a list", {
+  usrDef <- gsplot() %>% 
+    points(x=1, y=2, side=c(3,2), legend.name="Points 1", cex=3, xlab='cat')
+  lim(usrDef) <- list('side.3'=c(0.2,NA))
+  expect_true(usrDef$side.3$usr.lim[1])
+  expect_false(usrDef$side.3$usr.lim[2])
+})
+
+test_that("can't set a side that doesn't exist", {
+  usrDef <- gsplot() %>% 
+    points(x=1, y=2, side=c(3,2), legend.name="Points 1", cex=3, xlab='cat')
+  expect_error(lim(usrDef, 'side.1') <- c(0,1))
+  
+  expect_error(lim(usrDef) <- list('side.1'=c(0.2,1)))
+})
+
+test_that("can't specify side and provide a list", {
+  usrDef <- gsplot() %>% 
+    points(x=1, y=2, side=c(3,2), legend.name="Points 1", cex=3, xlab='cat')
+  
+  expect_error(lim(usrDef, side=1) <- list('side.1'=c(0.2,1)))
 })
