731c2407a9d289805dfd13dca85e7a5ebf5de2fc
Jordan S Read <jread@usgs.gov>
2016-07-06 10:06:03 -0500

    better about dealing w/ different inputs

--- R/mutate-gsplot.R
+++ R/mutate-gsplot.R
@@ -27,37 +27,35 @@
 
 #' @export
 `lim<-.gsplot` <- function(object, value, side=NULL){
-  if (is.null(side)){
-    # // value must be a list
-    stopifnot(is(value,'list'))
-    object <- `lim<-.gsplot_list`(object, value)
+  
+  if (is(value, 'list')){
+    object <- `lim<-.gsplot_list`(object, value, side)
   } else {
-    # // deal w/ setting reverse back to FALSE?
-    if (is(value, 'list')){
-      stop('cannot use value as a list and specify side argument', call. = FALSE)
-    }
-    
-    if (is(side,'numeric')){
+    if (is.null(side)){
+      stop('side must be specified unless using a list', call. = FALSE)
+    } else if (is(side,'numeric')){
       side.name <- as.side_name(side)
     } else if (is(side,'character')){
       side.name <- side
     } else {
       stop('class', class(side), 'for side is not supported')
     }
-
     object <- `lim<-.gsplot_named_side`(object, value, side.name)
-    
-  }
+  } 
   return(object)
 }
 
-`lim<-.gsplot_list` <- function(object, value){
+`lim<-.gsplot_list` <- function(object, value, side){
+  if (!is.null(side)){
+    stop('cannot specify side when using a list as input', call. = FALSE)
+  }
   side.names <- names(value)
   for (side.name in side.names){
     object <- `lim<-.gsplot_named_side`(object, value=value[[side.name]], side.name)
   }
   return(object)
 }
+
 `lim<-.gsplot_named_side` <- function(object, value, side.name){
   stopifnot(length(side.name) == 1, is(side.name,'character'), side.name %in% side_names(object))
   # // hack to deal w/ lim(myplot, side=1)[1] <- 0.25 case w usr.lim setting
