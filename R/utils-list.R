ac12b3c472db4542036a782152f94dc418011f15
Lindsay Carr <lcarr@usgs.gov>
2016-11-07 13:31:11 -0600

    append without losing timezones in strip_pts

--- R/utils-list.R
+++ R/utils-list.R
@@ -34,18 +34,24 @@ strip_pts <- function(list, param){
   for (v in param){
     if (v %in% names(list) && !inherits(list[[v]], c('function','formula'))) {
       v.vals <- list[[v]]
-      out <- append(out, v.vals)
+      # append to out without losing timezones when it is a date/POSIX
+      if(is.null(out)){
+        out <- v.vals
+      } else {
+        out <- append(out, v.vals)
+      }
       out.class <- ifelse(!all(is.na(v.vals)), class(v.vals), out.class)
     } else {
       if (any(sapply(list, is.list))){
         u.list <- unname_c(list[sapply(list, is.list)])
         if(v %in% names(u.list)) {
           v.vals <- u.list[[v]]
-          out <- append(out, v.vals)
+          out <- append_keepclass(out, v.vals)
           out.class <- ifelse(!all(is.na(v.vals)), class(v.vals), out.class)
         } else if (any(sapply(u.list, function(x) any(names(x) %in% v)))) {
           v.vals <- u.list[[which(sapply(u.list, function(x) any(names(x) %in% v)))]][[v]]
-          out <- append(out, v.vals)
+          # append to out without losing timezones when it is a date/POSIX
+          out <- append_keepclass(out, v.vals)
           out.class <- ifelse(!all(is.na(v.vals)), class(v.vals), out.class)
         } else {
           out <- append(out, NA)
@@ -76,3 +82,13 @@ strip_pts2 <- function(data, param) {
   return(out)
 }
 
+# append without losing timezones for the case where 
+# append.vals is POSIX and base.vals is NULL
+append_keepclass <- function(base.vals, append.vals){
+  if(is.null(base.vals)){
+    vals <- append.vals
+  } else {
+    vals <- append(base.vals, append.vals)
+  }
+  return(vals)
+}
