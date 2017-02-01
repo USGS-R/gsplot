e123a0aff9fe4e8f7cd59ad5a98466e23631ec2f
Lindsay Carr <lcarr@usgs.gov>
2017-02-01 12:38:12 -0600

    test for view() w/ passing log in for diff sides

--- tests/testthat/test-pass_view_args.R
+++ tests/testthat/test-pass_view_args.R
@@ -15,3 +15,14 @@ test_that("frame.plot", {
   expect_false(gs$global$config$frame.plot)
   expect_false(gs$side.1$axes)
 })
+
+test_that("side", {
+  
+  gs <- gsplot() %>% points(1,2, side=3) %>% view(log='x')
+  expect_true(gs$side.1$log)
+  expect_false(gs$side.3$log)
+  
+  gs <- gsplot() %>% points(1,2) %>% view(side=3, log='x')
+  expect_true(gs$side.3$log)
+  expect_false(gs$side.1$log)
+})
