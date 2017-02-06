2f00df2b2af39713a0f9aff72aeebb532c2aa176
Jordan Walker <jiwalker@usgs.gov>
2017-02-06 17:48:53 -0600

    Filling in lazy evals

--- R/date_axis.R
+++ R/date_axis.R
@@ -25,13 +25,67 @@ date_axis.gsplot <- function(object, ..., lab.pos="tick", tick.int=NULL, snap.to
   if (!is.null(at) && !is.null(tick.int)) {
     warning("cannot specify both at and tick.int, at will be ignored")
   }
-  
+  if (!is.null(tick.int)) {
+    ticksAt <- lazy(function() {
+      # calc ticks
+      limit <- lim(object, side)[[1]] # limits should be shared
+      # TODO: handle -ly on tick.int
+      seq(limit[1], limit[2], tick.int)
+    })
+  } else if (!is.null(at)) {
+    ticksAt <- at
+  } else {
+    ticksAt <- NULL
+  }
+
+  labelsAt <- lazy(function(){
+    labels <- NULL
+    limit <- lim(object, side)[[1]]
+    if (lab.pos == "tick") {
+      labels <- ticksAt
+    } else if (lab.pos == "interval") {
+      prev = limit[1]
+      for (intBnd in c(ticksAt, limit[2])) {
+        if (intBnd != prev) {
+          labels <- c(labels, prev + (intBnd-prev)/2)
+        }
+        prev = intBnd
+      }
+    } else {
+      stop("lab.pos must be \"tick\" or \"interval\"")
+    }
+    return(labels)
+  })
+  snapTo <- lazy(function() {
+    old.lim <- lim(object, side)[[1]]
+    # TODO: handle start.on.monday=TRUE use %u instead of %w and offset 1
+    limit <- switch(snap.to,
+           "day" = c(as.POSIXct(format(old.lim[1]), '%Y-%m-%dT00:00:00'), 
+                     as.POSIXct(format(old.lim[2], '%Y-%m-%dT23:59:59'))),
+           "week" = c(as.Date(old.lim[1]) - format(old.lim[1], '%w'), # 0 = sunday
+                      as.Date(old.lim[2]) + (6 - format(old.lim[2], '%w'))), # 6 = saturday
+           "month" = c(as.Date(format(old.lim[1]), '%Y-%m-01'),
+                      as.Date(format(old.lim[2]), paste0('%Y-%m-', ndays(old.lim[2])))), 
+           "quarter" = c(), # NOT YET IMPLEMENTED
+           "year" = c(as.Date(format(old.lim[1], '%Y-01-01')),
+                      as.Date(format(old.lim[2], '%Y-12-31'))),
+           "wateryear" = c(), # NOT YET IMPLEMENTED
+           "decade" = c(), # NOT YET IMPLEMENTED
+           old.lim # default
+    )
+  })
   # can we lazy eval 'at'
-  object <- axis(object, ..., at=seq(year[1], year[2], "months"), labels=FALSE)
-  object <- axis(object, ..., at=seq(year[1], year[2], "months")+15, tick=FALSE, append=TRUE)
+  object <- axis(object, ..., at=ticksAt, labels=FALSE)
+  object <- axis(object, ..., at=labelsAt, tick=FALSE, append=TRUE)
 }
 
 date_axis.default <- function(lab.pos="tick", tick.int=NULL, snap.to=NULL, ...) {
   warning("date_axis is not implemented for base graphics plots")
   return()
+}
+
+ndays <- function(date) {
+  last_days <- 28:31
+  rev(last_days[which(!is.na(as.Date(paste(substr(date, 1, 8),
+                                           last_days, sep = ''), '%Y-%m-%d')))])[1]
 }
