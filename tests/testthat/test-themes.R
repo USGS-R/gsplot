acb55f16c8af5f16ba3831f26f1e649ad466974a
Laura DeCicco <ldecicco@usgs.gov>
2017-02-14 22:08:53 -0600

    Add a few tests

--- tests/testthat/test-themes.R
+++ tests/testthat/test-themes.R
@@ -6,7 +6,7 @@ gs <- gsplot(theme = theme.hadley) %>%
        points(1:10, 1:10, xlab="Index")
 expect_length(gs, 6)
 expect_true(gs$global$config$config.file)
-expect_equal(gs$view.1.2$points$col, "brown")
+expect_equal(gs$view.1.2$points$pch, 16)
 
   
 })
