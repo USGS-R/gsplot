f689e53f12ca0cda29a1bc5a72c7e2284eadd08e
Jordan S Read <jread@usgs.gov>
2017-03-22 09:31:57 -0500

    for #437 w/ some docs too

--- R/axis.R
+++ R/axis.R
@@ -4,6 +4,12 @@
 #'
 #' @param object gsplot object
 #' @param \dots Further graphical parameters may also be supplied as arguments. See 'Details'.
+#' @param n.minor number of minor ticks between major ticks
+#' @param tcl.minor tick length for minor ticks (used if \code{n.minor} > 0). Use NA (the default) to have this 
+#' calculated automatically as half of the value for \code{par('tcl')} at the time of rendering the axis. 
+#' @param reverse flip the orientation of the axis?
+#' @param append replace or append an existing axis for this side (logical)
+#' 
 #' 
 #' @details Additional graphical parameter inputs: 
 #' \itemize{
@@ -64,40 +70,51 @@ axis <- function(object, ...) {
   override("graphics", "axis", object, ...)
 }
 
-axis.gsplot <- function(object, ..., n.minor=0, tcl.minor=0.15, reverse=NULL, append=FALSE) {
+axis.gsplot <- function(object, ..., n.minor=0, tcl.minor=NA, reverse=NULL, append=FALSE) {
   
   fun.name <- "axis"
   
-  user_args <- filter_arguments(fun.name = fun.name, ..., 
-                                custom.config = object[["global"]][["config"]][["config.file"]])$call.args
+  args <- filter_arguments(fun.name = fun.name, ..., 
+                           custom.config = object[["global"]][["config"]][["config.file"]])
   
-  sides <- user_args[[fun.name]]$side
-  user_args[[fun.name]]$side <- NULL
-  user_args[[fun.name]]$n.minor <- n.minor
-  user_args[[fun.name]]$tcl.minor <- tcl.minor
+  if (length(args$extracted.args) > 0){
+    warning('axs.gsplot is not configured to support embedded function calls', call. = FALSE)
+  }
+  
+  user.args <- args$call.args
+  sides <- user.args[[fun.name]]$side
+  
+  if (is.null(sides)){
+    return(object)
+  }
+  
+  user.args[[fun.name]]$side <- NULL
+  user.args[[fun.name]]$n.minor <- n.minor
+  user.args[[fun.name]]$tcl.minor <- tcl.minor
+  
+  user.args[[fun.name]] <- append_replace(user.args[[fun.name]], args$option.args)
   
   for(side in sides){
     # append the side and give it defaults if it doesn't exist
-    if(append){
-      
-    } else {
+    if(!append){
       object <- modify_side(object, args = list(), side=side) 
     }
     
     object[[as.side_name(side)]][['usr.axes']] <- TRUE
-    object[[as.side_name(side)]][['axis']] <- append_replace(object[[as.side_name(side)]][['axis']], user_args[[fun.name]])
+    object[[as.side_name(side)]][['axis']] <- append_replace(object[[as.side_name(side)]][['axis']], user.args[[fun.name]])
     if (!is.null(reverse)){
       object[[as.side_name(side)]][['reverse']] <- reverse
     }
   }
+  
   class(object) <- 'gsplot'
   return(object)
   
 }
 
 draw_axis <- function(object, side.name){
-
-
+  
+  
   # method isn't made for multiple axis calls
   which.axis <- which(names(object[[side.name]]) == 'axis')
   if (length(which.axis) > 1){
@@ -107,7 +124,7 @@ draw_axis <- function(object, side.name){
       draw_axis(tmp, side.name)
     }
   }
-
+  
   axis.args <- object[[side.name]][['axis']]
   side.lim <- object[[side.name]][['lim']]
   
@@ -124,8 +141,7 @@ draw_axis <- function(object, side.name){
     
     tcl <- NULL
     if (exists('tcl.minor',axis.args)){
-      tcl <- axis.args$tcl.minor
-      
+      tcl <- ifelse(is.na(axis.args$tcl.minor), par('tcl')*0.5, axis.args$tcl.minor)
     }
     
     axis.args$n.minor <- NULL
